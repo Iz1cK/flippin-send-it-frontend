b40302d8b3ab5e1484e10fc37aa18f9fd9bd91a9
George Joubran <durd2001@gmail.com>
2022-02-04 07:50:28 +0200

    socket.io setup done with a working "chat"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { io } from "socket.io-client";
+import { useEffect, useState, React } from "react";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  // const socket = io("http://localhost:4000");
+  const [socket, setSocket] = useState(null);
+  const [textMessage, setTextMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    if (socket === null) {
+      setSocket(io("http://localhost:4000"));
+    }
+    if (socket) {
+      socket.on("message", (text) => {
+        setMessages(messages.concat(text));
+      });
+    }
+  });
+
+  const handleClick = (e) => {
+    socket.emit("message", textMessage);
+  };
+
+  return (
+    <div className="App">
+      Hello world
+      <ul>
+        {messages.map((message) => (
+          <li>{message}</li>
+        ))}
+      </ul>
+      <input
+        placeholder="message"
+        value={textMessage}
+        onChange={(e) => setTextMessage(e.target.value)}
+      />
+      <button onClick={handleClick}>Send</button>
+    </div>
+  );
 }
 
 export default App;

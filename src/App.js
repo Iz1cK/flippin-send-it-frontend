fb3477e0f51e7fed450bf0de248841b1d0a4c098
George Joubran <durd2001@gmail.com>
2022-02-06 16:04:57 +0200

    started working on authentication page

--- src/App.js
+++ src/App.js
@@ -1,43 +1,67 @@
 import "./App.css";
 import { io } from "socket.io-client";
 import { useEffect, useState, React } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/Home";
+import LogIn from "./components/LogIn";
+
+const checkLogin = () => {
+  return !!localStorage.getItem("access_token");
+};
+
+function RequireAuth({ children }) {
+  return !checkLogin() ? <Navigate to="/login" /> : children;
+}
 
 function App() {
-  // const socket = io("http://localhost:4000");
-  const [socket, setSocket] = useState(null);
-  const [textMessage, setTextMessage] = useState("");
-  const [messages, setMessages] = useState([]);
+  // const [socket, setSocket] = useState(null);
+  // const [textMessage, setTextMessage] = useState("");
+  // const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
-    if (socket === null) {
-      setSocket(io("http://localhost:4000"));
-    }
-    if (socket) {
-      socket.on("message", (text) => {
-        setMessages(messages.concat(text));
-      });
-    }
-  });
+  // useEffect(() => {
+  //   if (socket === null) {
+  //     setSocket(io("http://localhost:4000"));
+  //   }
+  //   if (socket) {
+  //     socket.on("message", (text) => {
+  //       setMessages(messages.concat(text));
+  //     });
+  //   }
+  // });
 
-  const handleClick = (e) => {
-    socket.emit("message", textMessage);
-  };
+  // const handleClick = (e) => {
+  //   socket.emit("message", textMessage);
+  // };
 
   return (
-    <div className="App">
-      Hello world
-      <ul>
-        {messages.map((message) => (
-          <li>{message}</li>
-        ))}
-      </ul>
-      <input
-        placeholder="message"
-        value={textMessage}
-        onChange={(e) => setTextMessage(e.target.value)}
-      />
-      <button onClick={handleClick}>Send</button>
-    </div>
+    <>
+      {/* <div className="App">
+        Hello world
+        <ul>
+          {messages.map((message) => (
+            <li>{message}</li>
+          ))}
+        </ul>
+        <input
+          placeholder="message"
+          value={textMessage}
+          onChange={(e) => setTextMessage(e.target.value)}
+        />
+        <button onClick={handleClick}>Send</button>
+      </div> */}
+      <Routes>
+        <Route
+          exact
+          path="/home"
+          element={
+            <RequireAuth>
+              <Home />
+            </RequireAuth>
+          }
+        />
+        <Route exact path="/login" element={<LogIn />} />
+      </Routes>
+    </>
   );
 }
 

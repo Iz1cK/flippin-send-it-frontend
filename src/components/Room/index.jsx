c6691fbd9dab6aebaabb7327f5aa00add8478f5a
George Joubran <durd2001@gmail.com>
2022-03-21 10:49:28 +0200

    added some input and button code

--- src/components/Room/index.jsx
+++ src/components/Room/index.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import style from "./room.module.css";
+import { io } from "socket.io-client";
 
 const API_URL = `http://localhost:4000/api`;
 
 export default function Room(props) {
+  const [socket, setSocket] = useState(null);
   const [messages, setMessages] = useState([]);
   const [userData, setUserData] = useState({});
   const [otherData, setOtherData] = useState({});
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-    let axiosConfig = {
+    const axiosConfig = {
       headers: {
         "Content-Type": "application/json",
         authorization: "Bearer " + localStorage.getItem("access_token"),
@@ -38,46 +41,63 @@ export default function Room(props) {
         ).data
       );
     }
+    updateData();
   }, []);
+
+  const sendMessageHandler = (e) => {
+    console.log(message);
+  };
 
   return (
     <>
       <h1 className={style.centerText}>Messages:</h1>
-      <div className={style.chatWindow}>
-        <ul className={style.noBullets}>
-          {messages
-            ? messages.map((message, index) => (
-                <li
-                  key={index}
-                  className={
-                    message.userid === userData.userid
-                      ? style.rightText
-                      : style.leftText
-                  }
-                >
-                  {message.userid === userData.userid ? (
-                    <div className={style.messageBox}>
-                      <span className={style.msgFrom}>
-                        {userData.firstname} {userData.lastname}:
-                      </span>
-                      <span className={style.msgMessage}>
-                        {message.message}
-                      </span>
-                    </div>
-                  ) : (
-                    <div className={style.messageBox}>
-                      <span className={style.msgFrom}>
-                        {otherData.firstname} {otherData.lastname}:
-                      </span>
-                      <span className={style.msgMessage}>
-                        {message.message}
-                      </span>
-                    </div>
-                  )}
-                </li>
-              ))
-            : null}
-        </ul>
+      <div className={style.flexBox}>
+        <div className={style.chatWindow}>
+          <ul className={style.noBullets}>
+            {messages
+              ? messages.map((message, index) => (
+                  <li
+                    key={index}
+                    className={
+                      message.userid === userData.userid
+                        ? style.rightText
+                        : style.leftText
+                    }
+                  >
+                    {message.userid === userData.userid ? (
+                      <div className={style.messageBox}>
+                        <span className={style.msgFrom}>
+                          {userData.firstname} {userData.lastname}:
+                        </span>
+                        <span className={style.msgMessage}>
+                          {message.message}
+                        </span>
+                      </div>
+                    ) : (
+                      <div className={style.messageBox}>
+                        <span className={style.msgFrom}>
+                          {otherData.firstname} {otherData.lastname}:
+                        </span>
+                        <span className={style.msgMessage}>
+                          {message.message}
+                        </span>
+                      </div>
+                    )}
+                  </li>
+                ))
+              : null}
+          </ul>
+        </div>
+        <div className={style.chatBox}>
+          <input
+            type="text"
+            className={style.chatInput}
+            onChange={(e) => setMessage(e.target.value)}
+          ></input>
+          <button onClick={sendMessageHandler} className={style.sendButton}>
+            Send
+          </button>
+        </div>
       </div>
     </>
   );
